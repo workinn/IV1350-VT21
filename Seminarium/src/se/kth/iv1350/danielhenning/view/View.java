8d5fdb66a6341c6a8ada3c4fe1afe423cdadf6a9
HenningWigforss <henning.wigforss@gmail.com>
2021-05-05 20:09:42 +0200

    Printout to view
    Ändrade så att view skriver ut allt nu (utom kvitto) la dessutom till text som säger i varje steg vad vi gör.

--- Seminarium/src/se/kth/iv1350/danielhenning/view/View.java
+++ Seminarium/src/se/kth/iv1350/danielhenning/view/View.java
@@ -18,26 +18,69 @@ public class View {
    */
   public View(Controller controller) {
     this.controller = controller;
-
-    SaleDTO currentStateOfSale;
     String appleID = "1337";
     String colaID = "1";
+    String noneExcistingItem ="111111";
     String existingCustomerID = "1337";
     String noneExistingCustomerID = "10";
+    SaleDTO currentStateOfSale;
 
+    System.out.println("->Starting new Sale\n");
     controller.startSale();
+    System.out.println("->Adding Apple");
     currentStateOfSale = controller.addItem(appleID);
-    currentStateOfSale.printSale();
+    printSale(currentStateOfSale);
+    System.out.println("->Adding Qunatity 10 (+9)");
     currentStateOfSale = controller.addQuantity(10);
-    currentStateOfSale.printSale();
+    printSale(currentStateOfSale);
+    System.out.println("->Adding Cola");
     currentStateOfSale = controller.addItem(colaID);
-    currentStateOfSale.printSale();
+    printSale(currentStateOfSale);
+    System.out.println("->Adding Qunatity 5 (+4)");
     currentStateOfSale = controller.addQuantity(5);
-    currentStateOfSale.printSale();
+    printSale(currentStateOfSale);
+    System.out.println("->Adding none existing item");
+    currentStateOfSale = controller.addItem(noneExcistingItem);
+    printSale(currentStateOfSale);
+    System.out.println("->End Sale");
     currentStateOfSale = controller.endSale();
-    currentStateOfSale.printSale();
+    printSale(currentStateOfSale);
+    System.out.println("->Send Discount Request");
     currentStateOfSale = controller.requestDiscount(existingCustomerID);
-    currentStateOfSale.printSale();
+    printSale(currentStateOfSale);
+    System.out.println("->Make Payment 500kr");
     currentStateOfSale = controller.payment(500);
   }
+
+
+  public void printSale(SaleDTO currentStateOfSale) {
+    if(currentStateOfSale.getLastItemFound() == false) {
+      System.out.println("--------------------------");
+      System.out.println("|INVALID ITEM IDENTIFIER!|");
+      System.out.println("--------------------------");
+    }
+    System.out.println("---------------------------------------------------------------");
+    System.out.println("| Row | Description | ID  | Quantity  | Discount  | Row Price |");
+    System.out.println("---------------------------------------------------------------");
+    for(int i = 0; i < currentStateOfSale.getItemRows().size(); i++) {
+      System.out.println("|  " + (i + 1) + "  |   " + currentStateOfSale.getItemRows().get(i).toString());
+      System.out.println("---------------------------------------------------------------");
+    }
+    System.out.println("Discount on whole sale: " + currentStateOfSale.getDiscountOnWholeSale());
+    System.out.println("Running Total: " + currentStateOfSale.getRunningTotal() + "             Number Of Items: " + currentStateOfSale.getNumberOfItems());
+    System.out.println("Amount Paid: " + currentStateOfSale.getAmountPaid() + "               Change: " + currentStateOfSale.getChange());
+    System.out.print("Date: " + currentStateOfSale.getDateTime().getYear() + "-" + currentStateOfSale.getDateTime().getMonth().getValue() + "-" + currentStateOfSale.getDateTime().getDayOfMonth());
+
+    int minute = currentStateOfSale.getDateTime().getMinute();
+    String min;
+
+    if(minute < 10) {
+      min = "0" + minute;
+    } else{
+      min = "" + minute;
+    }
+
+    System.out.println("                  Time: " + currentStateOfSale.getDateTime().getHour() + ":" + min + "\n");
+  }
+
 }
